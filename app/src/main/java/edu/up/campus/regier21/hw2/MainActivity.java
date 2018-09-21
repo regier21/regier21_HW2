e0338c43760780c0acbd057df57186f3a7fb75f8
Ryan Regier <ryanr2302@gmail.com>
2018-09-21 14:13:16 -0700

    Added self as author for project in MainActivity.java Fixed an IDE render problem by modifying styles.xml Decremented minimum SDK version

--- app/src/main/java/edu/up/campus/regier21/hw2/MainActivity.java
+++ app/src/main/java/edu/up/campus/regier21/hw2/MainActivity.java
@@ -3,6 +3,14 @@ package edu.up.campus.regier21.hw2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+/**
+ * @author Ryan Regier
+ * @version 1.0
+ *
+ * This class is the Activity that is the sole entry point for the program.
+ * It serves as the controller for the application.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
